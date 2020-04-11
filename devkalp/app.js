ccaa533f998b002085bad612596e4ea7cbd966a0
dimitriskalp <39502873+dimitriskalp@users.noreply.github.com>
2020-04-11 22:24:18 +0300

    Add files via upload

---
+++ devkalp/app.js
@@ -0,0 +1,31 @@
+const navSlide = () => {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav-links");
+  const navLinks = document.querySelectorAll(".nav-links li");
+  //Toggle key
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
+
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forward  ${
+          index / 7 + 0.3
+        }s`;
+      }
+    });
+    burger.classList.toggle("toggle");
+  });
+};
+navSlide();
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-8vh";
+  }
+  prevScrollpos = currentScrollPos;
+};
